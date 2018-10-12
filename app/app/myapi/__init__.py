c703b615b28dcdabbc20c6fc5e0295f59d125477
Tommy Carpenter <tommy@research.att.com>
2018-10-12 10:52:43 -0400

    Logging improvement

--- app/app/myapi/__init__.py
+++ app/app/myapi/__init__.py
@@ -13,7 +13,7 @@ def get_module_logger(mod_name):
     # 100MB log, up to 5
     handler = logging.handlers.RotatingFileHandler("/opt/logs/log.txt", maxBytes=100000000, backupCount=5)
     handler2 = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s   %(name)s   %(levelname)-8s   %(message)s')
+    formatter = logging.Formatter('[%(asctime)-20s] [%(name)-20s] [%(levelname)-8s] %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.addHandler(handler2)
