303aec84b72d2ed85b7cd703a5e0e7ab8fd184f3
Tommy Carpenter <tommy@research.att.com>
2018-10-12 11:53:12 -0400

    Improvements in logging, update to python 37 as latest

--- app/app/main.py
+++ app/app/main.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 import connexion
 from myapi import get_module_logger
+from myapi import controller
 
 _logger = get_module_logger(__name__)
 
 app = connexion.App(__name__, specification_dir='.')
 app.add_api('swagger.yaml', arguments={'title': 'My API'})
 
-_logger.debug("Starting Application")
-
 if __name__ == "__main__":
     # Entrypoint when testing locally. UWSGI DOES NOT ENTER HERE
     app.run(host='0.0.0.0', debug=True, port=10000)
+else:
+    # Entrypoint in UWSGI
+    _logger.debug("Setting up file based logging")
+    controller.logger = get_module_logger("controller")
+    _logger.debug("Starting Application in UWSGI")
--- app/app/myapi/controller.py
+++ app/app/myapi/controller.py
@@ -1,15 +1,21 @@
 import connexion
 from flask import Response
+import logging
+
+# will be set in uwsgi later
+logger = logging.getLogger()
 
 
 def foo_handler(bar):
     """demonstration of path parameter, see swagger"""
+    logger.debug("foo handler called")
     return Response(response=bar,
                     status=200)
 
 
 def baz_handler():
     """demonstration of function with post body, see swagger"""
+    logger.debug("baz handler called")
     body = connexion.request.json
     query_string = body.get("query_string")
     return Response(response=query_string,
